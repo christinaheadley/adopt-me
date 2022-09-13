fbdd1ed9afa6b247260683254c8c4b1fa401b774
christina <chrstnaheadley@gmail.com>
2022-09-13 16:09:50 -0400

    map data to component

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,35 +1,67 @@
 import { useState } from "react";
 
-const SearchParams = () => {
-  // const location = "Chapel Hill, NC";
-  // if don't add hook useState and ability to set Location, field is immutable/ cannot type in it or change it
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
-  // Do not have conditional hooks!
-  // Hooks are dependent on order listed and have to be called in the same order every time. If statements and loops can create variable numbers of hooks to be called and interfere with that order.
-  // ESLint react-hooks will montior
+const SearchParams = () => {
   const [location, setLocation] = useState("");
+  const [animal, setAnimal] = useState("");
+  const [breed, setBreed] = useState("");
+  const breeds = [
+    "Australian Sheperd/ Border Collie mix",
+    "medium-hair",
+    "orange tabby",
+    "poodle",
+  ];
   return (
     <div className="search-params">
-      {/* Below is example of controlled form, NOT a best practice unless you need it, for example with an area code or zip code */}
-      {/* <form>
-        <label htmlFor="location">
-          Location {location}
-          <input
-            id="location"
-            value={location}
-            placeholder="Location"
-            // Anytime a change event happens on this input, going to call function below, going to pull out target.value of that event (target is input), then going to call setLocation (line 6), which is going to update the state. React re-renders with new state.
-            // To verify, add {location} as on-screen text (line 11) and you can see it update in real time as you type in the search box
-            onChange={(e) => setLocation(e.target.value)}
-          />
-        </label>
-        <button>Submit</button>
-      </form> */}
-      {/* uncontrolled input/form is the preferred way: */}
-      <form onSubmit={somethingHere}>
+      <form>
         <label htmlFor="location">
           Location
           <input id="location" placeholder="Location" />
+        </label>
+        <label htmlFor="animal">
+          Animals
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => {
+              setAnimal(e.target.value);
+              setBreed("");
+            }}
+            // set onBlur bc sometimes when browsers navigate away from a select, they do not fire the change event
+            onBlur={(e) => {
+              setAnimal(e.target.value);
+              setBreed("");
+            }}
+          >
+            <option></option>
+            {ANIMALS.map((animal) => (
+              <option key={animal} value={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          Breeds
+          <select
+            disabled={!breeds.length}
+            id="breed"
+            value={breed}
+            onChange={(e) => {
+              setBreed(e.target.value);
+            }}
+            onBlur={(e) => {
+              setBreed(e.target.value);
+            }}
+          >
+            <option />
+            {breeds.map((breed) => (
+              <option key={breed} value={breed}>
+                {breed}
+              </option>
+            ))}
+          </select>
         </label>
         <button>Submit</button>
       </form>
